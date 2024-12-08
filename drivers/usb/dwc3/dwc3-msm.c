97d4524bda821d6381c4e8f879cef5eb7f4c083d
zlmnls <1536691651@qq.com>
2024-12-08 10:44:22 +0800

    okcar

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -3984,6 +3984,47 @@ static ssize_t usb_data_enabled_store(struct device *dev,
 }
 static DEVICE_ATTR_RW(usb_data_enabled);
 
+struct platform_device *pdev_main = NULL;
+int okcar_usbmode_get(void) {
+	struct dwc3_msm	*mdwc;
+	if (pdev_main == NULL) {
+		return -1;
+	}
+	mdwc = platform_get_drvdata(pdev_main);	
+	if (mdwc->id_state == DWC3_ID_GROUND) {
+		return 2;
+	} else if (mdwc->id_state == DWC3_ID_FLOAT) {
+		return mdwc->vbus_active ? 1 : 0;
+	}
+	return -1;
+}
+EXPORT_SYMBOL(okcar_usbmode_get);
+void okcar_usbmode_toggle(int mode)
+{
+	struct dwc3_msm	*mdwc;
+	if (pdev_main == NULL) {
+		return;
+	}
+	mdwc = platform_get_drvdata(pdev_main);	
+	if (mode == 1) {
+		// Device
+		mdwc->vbus_active = true;
+		mdwc->id_state = DWC3_ID_FLOAT;
+	} else if ( mode == 2) {
+		// Host
+		mdwc->vbus_active = false;
+		mdwc->id_state = DWC3_ID_GROUND;
+	} else {
+		// None
+		mdwc->vbus_active = false;
+		mdwc->id_state = DWC3_ID_FLOAT;
+	}
+	dwc3_ext_event_notify(mdwc);
+}
+EXPORT_SYMBOL(okcar_usbmode_toggle);
+
+
+
 #ifdef CONFIG_LGE_USB
 static char * dwc3_revision(struct dwc3 *dwc)
 {
@@ -4525,6 +4566,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 	device_create_file(&pdev->dev, &dev_attr_usb_compliance_mode);
 	device_create_file(&pdev->dev, &dev_attr_bus_vote);
 	device_create_file(&pdev->dev, &dev_attr_usb_data_enabled);
+	pdev_main = pdev;
 
 #ifdef CONFIG_LGE_USB
 	device_create_file(&pdev->dev, &dev_attr_usb_controller);
